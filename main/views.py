e65fa6cd3f990b1ab034aed13a0d9ee16e677109
Eduard Latypov <LatypovEdik1988@yandex.ru>
2024-02-03 15:28:14 +0300

    добавил приложение main отвечающее за главную страницу сайта

---
+++ main/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse, HttpRequest
+from django.shortcuts import render
+
+
+def index(request):
+    context = {
+        "title": "Home",
+        "description": "Это домашняя страница сайта - Home!",
+    }
+    return render(request, "main/index.html", context)
