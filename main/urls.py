8fa55c65ec6c9206cfb4942eed704051baa9c997
Eduard Latypov <LatypovEdik1988@yandex.ru>
2024-02-05 21:11:21 +0300

    добавил шаблон с информацией О НАС

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import index
+from .views import *
+
+app_name = "main"
 
 urlpatterns = [
-    path("", index, name="home"),
+    path("", index, name="index"),
+    path("about/", about, name="about"),
 ]
