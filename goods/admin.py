9df38e2534449f3a8f4ea2e91e627121219e44d3
Eduard Latypov <LatypovEdik1988@yandex.ru>
2024-02-06 22:02:28 +0300

    Создал 2 модели - Категория и Товары

--- goods/admin.py
+++ goods/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Categories
+
+
+@admin.register(Categories)
+class CategoriesAdmin(admin.ModelAdmin):
+    list_display = ["name", "slug"]
--- goods/models.py
+++ goods/models.py
@@ -2,9 +2,32 @@ from django.db import models
 
 
 class Categories(models.Model):
-    name = models.CharField(max_length=150, unique=True, verbose_name="Категория")
+    name = models.CharField(max_length=150, verbose_name="Название")
     slug = models.SlugField(max_length=200, unique=True, verbose_name="Слаг")
 
     class Meta:
         verbose_name = "Категория"
         verbose_name_plural = "Категории"
+
+
+class Products(models.Model):
+    name = models.CharField(max_length=150, verbose_name="Название товара")
+    slug = models.SlugField(max_length=200, unique=True, verbose_name="Слаг")
+    description = models.TextField(blank=True, null=True, verbose_name="Описание")
+    image = models.ImageField(
+        upload_to="%Y/%m/%d/", blank=True, null=True, verbose_name="Изображение"
+    )
+    price = models.DecimalField(
+        max_digits=7, decimal_places=2, default=0.00, verbose_name="Цена"
+    )
+    quantity = models.PositiveIntegerField(default=0, verbose_name="Количество")
+    category = models.ForeignKey(
+        Categories,
+        on_delete=models.CASCADE,
+        related_name="products",
+        verbose_name="Категория",
+    )
+
+    class Meta:
+        verbose_name = "Товар"
+        verbose_name_plural = "Товары"
