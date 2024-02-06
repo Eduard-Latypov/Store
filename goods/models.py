19f47e6bac6ed09a5011a15c83be622c66e5f642
Eduard Latypov <LatypovEdik1988@yandex.ru>
2024-02-06 21:34:45 +0300

    Некоторые дополнения в шаблонах

--- goods/models.py
+++ goods/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Categories(models.Model):
+    name = models.CharField(max_length=150, unique=True, verbose_name="Категория")
+    slug = models.SlugField(max_length=200, unique=True, verbose_name="Слаг")
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
--- goods/urls.py
+++ goods/urls.py
@@ -4,6 +4,6 @@ from .views import *
 app_name = "goods"
 
 urlpatterns = [
-    path("", catalog, name="catalog"),
+    path("", catalog, name="index"),
     path("product/", product, name="product"),
 ]
