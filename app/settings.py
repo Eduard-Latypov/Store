1e0f38557b0d0e3de167244ca2a68911a554b5ba
Eduard Latypov <LatypovEdik1988@yandex.ru>
2024-02-09 16:50:08 +0300

    Добавил БД Posgres и перенес все данные

--- app/settings.py
+++ app/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = "app.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "fake_store",
+        "USER": "store",
+        "PASSWORD": "12345",
+        "HOST": "localhost",
+        "PORT": 5432,
     }
 }
 
