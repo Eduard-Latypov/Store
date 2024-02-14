ed2c6eb1c167b2fac79de3cf41af1be6fc4e3760
Eduard Latypov <LatypovEdik1988@yandex.ru>
2024-02-14 15:27:41 +0300

    Добавил форму для оформления заказа

---
+++ orders/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+import re
+
+
+class CreateOrderForm(forms.Form):
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+    phone_number = forms.CharField()
+    requires_delivery = forms.ChoiceField(choices=[("0", False), ("1", True)])
+    delivery_address = forms.CharField(required=False)
+    payment_on_get = forms.ChoiceField(choices=[("0", "False"), ("1", "True")])
+
+    def clean_phone_number(self):
+        data = self.cleaned_data["phone_number"]
+
+        if not data.isdigit():
+            raise forms.ValidationError("Номер телефона должен содержать только цифры")
+
+        pattern = re.compile(r"^\d{10}$")
+        if not pattern.match(data):
+            raise forms.ValidationError("Неверный формат номера")
+
+        return data
